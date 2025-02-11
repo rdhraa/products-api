2b97f28a819428a0156cbf596aab667cb34e38ed
Ardhra <ardhra367617@gmail.com>
2025-02-11 21:35:39 +0530

    few lines changed

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express  = require('express')
 const mongoose  = require('mongoose')
 const jwt = require('jsonwebtoken')
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 const cors = require('cors')
 
 const app = express()
